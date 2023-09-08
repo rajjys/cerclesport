9301f1a5591cf0a2af2eb60523a70f57653dabf5
rajjys <rajjysrachid@gmail.com>
2023-09-08 13:32:11 +0200

    Added points brought by each game

--- pages/standings/index.js
+++ pages/standings/index.js
@@ -8,8 +8,11 @@ const Standings = () => {
     getAllGames().then((data) => setGames(data));
   }, []);
   if(games.length == 0) return <p>Loading</p> ///If has not loaded yet
-  const gamesbyTeams = getGamesByTeams(games); ///Assigning games by each team
-  const gamesByTeams = addWinLossEntries(gamesbyTeams); ///Adding winOrLoss entry depending if the team owning the game won or lost
+  const gamesByTeams = getGamesByTeams(games); ///Assigning games by each team
+  const gamesAndPoints = addWinLossEntries(gamesByTeams); ///Adding winOrLoss and points entries depending if the team owning the game won or lost
+  const gamesAndTeamStats = addTeamStats(gamesAndPoints);
+  console.log(gamesAndPoints);
+
   return (
     <div>Standings</div>
   )
@@ -28,23 +31,24 @@ function getGamesByTeams( games ){
   }, {});
 }
 function addWinLossEntries( gamesByTeams ){
-    const games = Object.fromEntries(
-      Object.entries(gamesByTeams).map(([team, gamesByTeam]) => 
-          [team, gamesByTeam.map((game) => (
-            {...game, winOrLoss : parseInt(game.scoreTeam1) > parseInt(game.scoreTeam2) ? 
-                                          (game.team1.shortName === team ? "win" : "loss") : 
-                                          (game.team2.shortName === team ? "win" : "loss")}))])
-    );
-    /* const keys = Object.keys(games);
-    ///console.log("=======================TEAMS AND GAMES===========================");
-    keys.forEach((team) => {
-      console.log("\n\n---------------" + team + "----------------\n")
-      games[team].map((game) => {
-        console.log(game.team1.name + " " + game.scoreTeam1 + " - " + game.scoreTeam2 + " " + game.team2.name);
-        console.log(team + " " + game['winOrLoss']);
-      })
-    });
-    console.log(games);*/
+  ///Here we add winOrLoss entry depending if the team owning the game has won it
+  ///We also add the points each game brought. 2 for win, 1 for loss, and 0 if it's a loss by forfeit
+  const games = Object.fromEntries(
+    Object.entries(gamesByTeams).map(([team, gamesByTeam]) => {
+      return [team, gamesByTeam.map((game) => {
+        const winOrLoss = parseInt(game.scoreTeam1) > parseInt(game.scoreTeam2) ? 
+          (game.team1.shortName === team ? "win" : "loss") : 
+          (game.team2.shortName === team ? "win" : "loss");
+        return {...game, winOrLoss,
+          points : (winOrLoss == "win") ? 2 : (game.gameState === "Forfeited") ? 0 : 1
+        };
+      })];
+    })
+  );
     return games;
+}
+
+function addTeamStats( gamesAndPoints ){
+  
 }
 export default Standings
