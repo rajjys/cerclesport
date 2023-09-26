8dae1b7cd759af2cabe4a9fd5c6a994684d22aac
rajjys <rajjysrachid@gmail.com>
2023-09-26 16:55:28 +0200

    Added teams list to the team page

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { getAllTeams } from '@/services';
+import Link from 'next/link';
+import React, {useState, useEffect} from 'react'
+import Image from 'next/image';
 
 const Teams = () => {
+
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    getAllTeams().then((data) => setTeams(data))
+  }, []);
+  console.log(teams);
   return (
-    <div>Teams</div>
+    teams.length == 0 ?
+    <p className='text-black'> Loading</p> :
+    <div className='text-black'>
+      <span className='text-xl font-bold text-indigo-900 m-8'>Equipes Division 1 - Version Masculin</span>
+      <div className='grid grid-cols-4 p-4'>
+        {teams.map((team, index) => 
+        (<Link href={`/team/${team.slug}`} className='col-span-4 md:col-span-2 lg:col-span-1 flex items-center rounded-md m-2 p-2 text-indigo-950 hover:border hover:border-red-200 hover:bg-gray-200 transition duration-300 ease-in-out ' key={index}>
+            <Image
+                  alt={team.shortName}
+                  unoptimized
+                  width="60"
+                  height="60"
+                  className='align-middle rounded-full'
+                  src={team.photo.url}
+                />
+                <span className='px-2 overflow-hidden whitespace-nowrap font-bold text-lg'>{team.name}</span>
+                <span className='text-gray-400 font-medium'>{`(${team.shortName})`}</span>
+        </Link>))}
+      </div>
+    </div>
   )
 }
 
--- services/index.js
+++ services/index.js
@@ -90,6 +90,21 @@ export const getGamesByTeam = async ( slug ) => {
   const result = await request(graphqlAPI, query, {slug});
       return result['games'];
 }
+export const getAllTeams = async () => {
+  const query = gql`
+      query TeamsQuery {
+        teams(last: 50, orderBy: name_ASC){
+          name
+          photo {
+            url(transformation: {image: {resize: {height: 60, width: 60}}})
+          }
+          slug
+          shortName
+        }
+      }`
+      const result = await request(graphqlAPI, query);
+      return result['teams'];
+}
 export const getTeamProfile = async ( slug ) => {
   const query = gql`
       query GetProfile($slug:String!){
