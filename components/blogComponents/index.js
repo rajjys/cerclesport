b6ec71f6699452f353cc3659e56a7961764a2bb5
rajjys <rajjysrachid@gmail.com>
2023-11-08 15:20:51 +0200

    Removed unsused components

--- components/blogComponents/index.js
+++ components/blogComponents/index.js
@@ -1,13 +1,4 @@
 export {default as PostCard} from './PostCard';
 export {default as PostWidget} from './PostWidget'
-export {default as Layout} from './Layout'
-export {default as Comments} from './Comments'
-export {default as CommentsForm} from './CommentsForm'
 export {default as PostDetail} from './PostDetail'
-export {default as Author} from './Author'
-export {default as FeaturedPostCard} from './FeaturePostCard'
 export {default as Loader} from './Loader'
-export {default as AdjacentPostCard} from './AdjacentPostCard'
-export {default as CategoryPostCard } from './CategoryPostCard'
-export {default as CustomLeftArrow } from './CustomLeftArrow'
-export {default as CustomRightArrow } from './CustomRightArrow'
