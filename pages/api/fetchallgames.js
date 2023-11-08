e93a61cfc733126309ba889bc0982ff5dd54b3a6
rajjys <rajjysrachid@gmail.com>
2023-11-08 16:59:43 +0200

    Debugging

--- pages/api/fetchallgames.js
+++ pages/api/fetchallgames.js
@@ -1,6 +1,13 @@
 import { promises as fs } from 'fs';
     export default async function handler (req, res) {
-      const games = await fs.readFile('data/games.json', 'utf8');
+      console.log("Calling api");
+      const games = await fs.readFile('data/games.json', 'utf8',(err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
       const parsedGames = JSON.parse(games);
       console.log(parsedGames);
       return res.status(200).json(parsedGames);
